758f1a93d431fd9c65bf13deddb99fe34a755395
Kamila Duda <kakus86@o2.pl>
2020-10-02 10:52:41 +0200

    mobile view change

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ if (window.scrollY > navigationTop) {
     navigation.classList.remove("sticky");
   }
 })
+const menuMobileButton = document.querySelector('.mobileMenu__button')
+  const menuMobile = document.querySelector('.mobileMenu')
+  menuMobileButton.addEventListener('click', () => {
+    menuMobileButton.classList.toggle('mobileMenu__button--open')
+    menuMobile.classList.toggle('mobileMenu--open')
+  })
+  menuMobile.addEventListener('click', () => {
+    menuMobile.classList.toggle('mobileMenu--open')
+    menuMobileButton.classList.toggle('mobileMenu__button--open')
+  })
